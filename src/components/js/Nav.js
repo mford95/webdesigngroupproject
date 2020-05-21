efdd33c29f07b8bc5bac4f8e0a1c72da5121cb99
Matthew <mjford95@gmail.com>
2020-05-22 11:35:19 +1200

    added folders/files for

---
+++ src/components/js/Nav.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import "../css/Nav.css";
+import {Link} from "react-router-dom";
+
+export default function Nav() {
+    return (
+        <div className="nav-container">
+            <nav>
+
+            
+            </nav>
+        </div>
+    )
+}
