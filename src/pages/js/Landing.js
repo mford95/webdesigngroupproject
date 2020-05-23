be3696eaf72fccf2863959770a819057ecf82b33
Rupert <rupert4t@gmail.com>
2020-05-23 15:09:31 +1200

    basic structure for landing section created

--- src/pages/js/Landing.js
+++ src/pages/js/Landing.js
@@ -1,9 +1,24 @@
 import React from 'react'
+import "../css/Landing.css";
+
 
 export default function Landing() {
     return (
         <div>
-            
+            <div class="landing-section">
+                <div class="landing-section-text">
+                    <h1>Title</h1>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.</p>
+                </div>
+                <div class="shortcut-links">
+                <p>Lorem</p>
+                <p>Lorem</p>
+                <p>Lorem</p>
+                <p>Lorem</p>
+                </div>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p><button>View all</button>
+
+            </div>
         </div>
     )
 }
