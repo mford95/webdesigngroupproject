1527ce9fe0ad5e6c0e46665a405a5d6a89c314c9
Matthew <mjford95@gmail.com>
2020-05-27 10:33:54 +1200

    added nav-bar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React from 'react';
+import { BrowserRouter as Router, Route} from "react-router-dom";
 
+import Landing from './pages/js/Landing';
 import './App.css';
+import Nav from "./components/js/Nav";
 
 function App() {
   return (
     <div className="App">
-  
+   <Router>
+        <Nav />
+        <Route path="/Landing" component={Landing} />
+        
+     </Router>
     </div>
   );
 }
--- src/components/js/Nav.js
+++ src/components/js/Nav.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import "../css/Nav.css";
-import {Link} from "react-router-dom";
+import {NavLink} from "react-router-dom";
 
 export default function Nav() {
     return (
         <div className="nav-container">
             <nav>
-
+                <NavLink to="Landing" className="homeLogo" >Home</NavLink>
+                <NavLink to="Levels" className="NavLinks" activeStyle={{textDecoration: "underline"}}>Levels</NavLink>
+                <NavLink to="Resources" className="NavLinks" activeStyle={{textDecoration: "underline"}}>Resources</NavLink>
+                <NavLink to="Careers" className="NavLinks" activeStyle={{textDecoration: "underline"}}>Careers</NavLink>
             
             </nav>
         </div>
