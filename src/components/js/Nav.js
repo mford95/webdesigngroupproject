eb0856e9de1cf691990fa3ad10f1e3e329be48ea
Matthew <mjford95@gmail.com>
2020-06-15 22:48:30 +1200

    changed logo for nav

--- src/components/js/Nav.js
+++ src/components/js/Nav.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import "../css/Nav.css";
 import {NavLink} from "react-router-dom";
-
+import homeLogo from "../../assets/nav/logo.png"
 
 export default function Nav() {
     return (
         <div className="nav-container">
             <nav>
-                <NavLink to="Landing" className="homeLogo" >Home</NavLink>
+                <NavLink to="Landing" className="homeLogo" ><img className="logo" src={homeLogo} alt='' ></img></NavLink>
                 <NavLink to="Levels" className="NavLinks" activeStyle={{textDecoration: "underline"}}>Levels</NavLink>
                 <NavLink to="Resources" className="NavLinks" activeStyle={{textDecoration: "underline"}}>Resources</NavLink>
                 <NavLink to="Careers" className="NavLinks" activeStyle={{textDecoration: "underline"}}>Careers</NavLink>
