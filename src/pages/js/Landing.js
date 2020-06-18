f5a981aa5f19b5b4f2100ea6972f8483c43fe186
Rupert <rupert4t@gmail.com>
2020-06-19 11:41:45 +1200

    parallax start, hover functions

--- src/pages/js/Landing.js
+++ src/pages/js/Landing.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import "../css/Landing.css";
 
+import { ParallaxProvider } from 'react-scroll-parallax';
+
 import Fade from 'react-reveal/Fade';
 import Reveal from 'react-reveal/Reveal';
 
@@ -15,6 +17,12 @@ import industrialImg from "../../assets/industrialImg.png";
 import mediaImg from "../../assets/mediaImg.png";
 import interactionImg from "../../assets/interactionImg.png";
 import communicationImg from "../../assets/communicationImg.png";
+
+
+import interactionMobile from "../../assets/interactionMobile.png";
+import interactionSplodge from "../../assets/interactionSplodge.png";
+
+
 
 
 export default function Landing() {
@@ -40,9 +48,9 @@ export default function Landing() {
             
             <Stats />
 
-            <Fade left duration={2000} delay={1000}>
+            {/* <Fade left duration={2000} delay={1000}>
                 <p>Markup that will be revealed on scroll</p>
-            </Fade>
+            </Fade> */}
 
             {/* <Reveal effect="fadeInUp"> */}
 
@@ -58,15 +66,17 @@ export default function Landing() {
                         </Fade>
 
                     </div>
+                    <Fade bottom duration={1500} fraction={0.1}>
+
                     <div className="major-main-content-text">
                         <div className="major-main-content-text-align">
-                        <Fade bottom duration={2000} fraction={0.1}>
                             <p>Learn how to develop original, useful and meaningful products that enrich our daily lives. Explore the complex social and cultural considerations that go into creating good design.</p>
                             <button className="major-main-content-industrial-button">Learn more</button>
-                            </Fade>
-
                         </div>
+
                     </div>
+                    </Fade>
+
                 </div>
             </div>
 
@@ -75,10 +85,16 @@ export default function Landing() {
 
 
             <div className="major-description">
+            <Fade bottom duration={2000} fraction={0.1}>
                 <h2>Media</h2>
+            </Fade>
+
                 <div className="major-main-content">
                     <div className="image">
+                    <Fade bottom duration={1500} fraction={0.1}>
                         <img src={mediaImg} alt="playstation controller with red splodge" />
+                        </Fade>
+
                     </div>
                     <div className="major-main-content-text">
                         <div className="major-main-content-text-align">
@@ -121,6 +137,117 @@ export default function Landing() {
 
 
 
+
+
+
+
+
+            <div className="major-description">
+                <h2>Communication</h2>
+                <div className="major-main-content">
+                    <div className="image">
+                        <div className="la">
+                            <img className="i-image" src={interactionMobile} alt="mobile phone with interface on it with yellow splodge" />
+                        </div>
+                    </div>
+                    <div className="major-main-content-text">
+                        <div className="major-main-content-text-align">
+                            <p>Learn to communicate effective visual messages, information and ideas that contribute to a successful design product.</p>
+                            <button className="major-main-content-communication-button">Learn more</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+
+
+
+
+
+
+
+
+
+
+
+            <div className="major-description">
+                <h2>Interaction</h2>
+                <div className="major-main-content">
+                    {/* <div className="image"> */}
+                        <div className="major-main-content-image">
+                            <div>
+                            <img className="i-image" src={interactionMobile} alt="mobile phone with interface on it with yellow splodge" />
+                            {/* <div class="overlay"> */}
+                                <img className="s-image" src={interactionSplodge} alt="mobile phone with interface on it with yellow splodge" />
+                            {/* </div> */}
+                            </div>
+                        {/* </div> */}
+
+                    </div>
+                    <div className="major-main-content-text">
+                        <div className="major-main-content-text-align">
+                            <p>Study how humans connect and interact with a vast range of physical and digital systems. Learn to envision how people experience products—from real objects to digital interactions with apps and websites—and bring that vision to life in ways that feel inspired, refined, and even magical.</p>
+                            <button className="major-main-content-interaction-button">Learn more</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+
+
+
+
+
+
+
+
+
+            <ParallaxProvider>
+
+            <div className="major-description">
+                <h2>Interaction</h2>
+                <div className="major-main-content">
+                    {/* <div className="image"> */}
+                        <div className="major-main-content-image">
+                        <div className="holder">
+
+                            <div className="init">
+                                <div class="one">
+                                    <img className="" src={interactionSplodge} alt="mobile phone with interface on it with yellow splodge" />
+                                </div>
+
+                                <div class="one">
+                                    <img className="" src={interactionMobile} alt="mobile phone with interface on it with yellow splodge" />
+                                </div>
+                            </div>
+                        </div>
+
+                    </div>
+                    <div className="major-main-content-text">
+                        <div className="major-main-content-text-align">
+                            <p>Study how humans connect and interact with a vast range of physical and digital systems. Learn to envision how people experience products—from real objects to digital interactions with apps and websites—and bring that vision to life in ways that feel inspired, refined, and even magical.</p>
+                            <button className="major-main-content-interaction-button">Learn more</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            </ParallaxProvider>
+
+
+
+
+
+
+
+            <div class="overlay">
+    <div class="text">Hello World</div>
+  </div>
+
+{/* <div className="wrap">
+  <img className="i-image" src={interactionMobile} alt="mobile phone with interface on it with yellow splodge" />
+  <img className="s-image" src={interactionSplodge} alt="mobile phone with interface on it with yellow splodge" />
+  </div> */}
         </div>
     )
 }
