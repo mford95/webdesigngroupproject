e56eb6dd9c41d5357ad01c0b15cacac9f4b44a68
scottjowseydesign <scottjowseydesign@gmail.com>
2020-06-18 16:06:56 +1200

    Created Course Page Structure

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Route} from "react-router-dom";
 import Landing from './pages/js/Landing';
 import './App.css';
 import Nav from "./components/js/Nav";
+import CoursePage from './pages/js/CoursePage';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
    <Router>
         <Nav />
         <Route path="/Landing" component={Landing} />
+        <Route path="/CoursePage" component={CoursePage} />
         
      </Router>
     </div>
--- src/pages/js/InteractionPapers.js
+++ src/pages/js/InteractionPapers.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import '../css/InteractionPapers.css';
 
 export default function InteractionPapers() {
     return (
