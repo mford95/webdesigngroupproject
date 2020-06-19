429ead9da5769c61fcac74ad07d43ef799ef6dd1
scottjowseydesign <scottjowseydesign@gmail.com>
2020-06-19 18:12:10 +1200

    Added Scroll Animations and Fades into Course Page

--- src/pages/js/InteractionPapers.js
+++ src/pages/js/InteractionPapers.js
@@ -2,12 +2,16 @@
 import React, { useContext, useEffect, useState } from "react";
 import { Context } from "../../context/GlobalContextProvider";
 import '../css/InteractionPapers.css';
+import { Link } from "react-router-dom";
 
 import HalfStar from '../../assets/half-star.png';
 import Star from '../../assets/star.png';
 import RankingBackdrop from '../../assets/ranking-backdrop.png';
 import YellowHalfStar from '../../assets/yellow-half-star.png';
 import YellowStar from '../../assets/yellow-star.png';
+
+
+import Fade from 'react-reveal/Fade';
 
 
 export default function InteractionPapers(props) {
@@ -33,53 +37,81 @@ export default function InteractionPapers(props) {
   return <div className="course-wrap">
   <div className="title-div">
       <div className="title-wrap">
-          <div className="return">
-              <h4>Return</h4>
-          </div>
-          <div className="div-title">
-              <h1>{selectedCourse.courseCode}</h1>
-          </div>
+          <Fade left>
+            <div className="return">
+                    <Link to ="/interactionMajor" style={{ textDecoration: 'none', color: 'black' }} >
+                        <h4>Return</h4>
+                    </Link>
+            </div>
+          </Fade>
+          <Fade left >
+            <div className="div-title">
+                <h1>{selectedCourse.courseCode}</h1>
+            </div>
+          </Fade>  
+          
           <div className="course-title-info">
+            <Fade left>
               <div className="title-text">
                   <h2>{selectedCourse.courseTitle}</h2>
                   <p>{selectedCourse.courseDescription}</p>
               </div>
-              <div className="course-points-div">
-                  <div className="points-block">
-                      <h4>Points</h4>
-                      <h1>{selectedCourse.points}</h1>
-                  </div>
-              </div>
+            </Fade>
+            <Fade right >
+                <div className="course-points-div">
+                    <div className="points-block">
+                        <h4>Points</h4>
+                        <h1>{selectedCourse.points}</h1>
+                    </div>
+                </div>
+            </Fade>
           </div>
       </div>
   </div>
   <div className="requirements-div">
-      <div className="div-title">
-          <h1>Requirements</h1>
-      </div>
-      <div className="paper-requirements">
+      <Fade>
+        <div className="div-title">
+            <h1>Requirements</h1>
+        </div>
+      </Fade>
+      <Fade left>
+        <div className="paper-requirements">
           <h2>75 points including 30 points from BDI or BAS Schedules OR Permission from Head of School</h2>
-      </div>
+        </div>
+      </Fade>
+      
   </div>
-  <div className="course-info-div">
-      <div className="info-box">
-          <h3>Course offered in:</h3>
-          <h1>{selectedCourse.courseOfferedIn}</h1>
-      </div>
-      <div className="info-box">
-          <h3>Campus:</h3>
-          <h1>{selectedCourse.campus}</h1>
-      </div>
-      <div className="info-box">
-          <h3>Duration:</h3>
-          <h1>{selectedCourse.duration}</h1>
-      </div>
-  </div>
-  <div className="projects-div">
-      <div className="div-title">
-          <h1>Projects</h1>
-      </div>
+  <Fade bottom >
+    <div className="course-info-div">
+        <Fade>
+            <div className="info-box">
+                <h3>Course offered in:</h3>
+                <h1>{selectedCourse.courseOfferedIn}</h1>
+            </div>
+        </Fade>
+        <Fade>
+            <div className="info-box">
+                <h3>Campus:</h3>
+                <h1>{selectedCourse.campus}</h1>
+            </div>
+        </Fade>
+        <Fade>
+            <div className="info-box">
+                <h3>Duration:</h3>
+                <h1>{selectedCourse.duration}</h1>
+            </div>
+        </Fade>
+            
+    </div>
+  </Fade>
 
+  <div className="projects-div">
+    <Fade>
+        <div className="div-title">
+            <h1>Projects</h1>
+        </div>
+    </Fade>
+    <Fade right cascade>   
       <div className="course-project">
           <div className="div-title">
               <h2>Project One </h2>
@@ -102,8 +134,9 @@ export default function InteractionPapers(props) {
               </div>
           </div>
       </div>
+    </Fade>
 
-
+    <Fade right cascade>
       <div className="course-project">
           <div className="div-title">
               <h2>Project Two </h2>
@@ -126,114 +159,123 @@ export default function InteractionPapers(props) {
               </div>
           </div>
       </div>
+    </Fade>
 
-
-      
-   
-
-      
-  </div>
-  <div className="course-ranking-div">
-                <div className="div-title">
-                    <h1>Course Ranking</h1>
-                </div>
-                <div className="ranking-div">
-                    <div className="backdrop">
-                        <img src= { RankingBackdrop } alt="Background to Course Ranking"/>
+    </div>
+    <Fade bottom>
+        <div className="course-ranking-div">
+                <Fade>
+                    <div className="div-title">
+                        <h1>Course Ranking</h1>
                     </div>
-                    <div className="ranking-box">
-                        <div className="ranking-title">
-                            <h1>4.5</h1>
-                            <h3>/5</h3>
+                </Fade>
+                <Fade right cascade>
+                    <div className="ranking-div">
+                        <div className="backdrop">
+                            <img src= { RankingBackdrop } alt="Background to Course Ranking"/>
                         </div>
-                        <div className="ranking-star">
-                            <div className="main-star">
-                                <img src= { Star }  alt="Star" />
+                        <div className="ranking-box">
+                            <div className="ranking-title">
+                                <h1>4.5</h1>
+                                <h3>/5</h3>
                             </div>
-                            <div className="main-star">
-                                <img src= { Star }  alt="Star" />
-                            </div>
-                            <div className="main-star">
-                                <img src= { Star }  alt="Star" />
-                            </div>
-                            <div className="main-star">
-                                <img src= { Star }  alt="Star" />
-                            </div>
-                            <div className="main-star">
-                                <img src= { HalfStar }  alt="Star" />
+                            <div className="ranking-star">
+                                <div className="main-star">
+                                    <img src= { Star }  alt="Star" />
+                                </div>
+                                <div className="main-star">
+                                    <img src= { Star }  alt="Star" />
+                                </div>
+                                <div className="main-star">
+                                    <img src= { Star }  alt="Star" />
+                                </div>
+                                <div className="main-star">
+                                    <img src= { Star }  alt="Star" />
+                                </div>
+                                <div className="main-star">
+                                    <img src= { HalfStar }  alt="Star" />
+                                </div>
                             </div>
                         </div>
+                            
                     </div>
-                        
-                    
+                </Fade>
 
-                </div>
+                <Fade bottom cascade>
                 <div className="feedback-div">
-                    <div className="feedback">
-                        <h3>Lorem</h3>
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore facere culpa incidunt cum necessitatibus, minima minus voluptate quis. Eveniet consequatur ea ex illum est beatae distinctio iure excepturi tenetur soluta.</p>
-                        <div className="star-rating">
-                            <div className="star">
-                                <img src= { YellowStar }  alt="Star" />
-                            </div>
-                            <div className="star">
-                                <img src= { YellowStar }  alt="Star" />
-                            </div>
-                            <div className="star">
-                                <img src= { YellowStar }  alt="Star" />
-                            </div>
-                            <div className="star">
-                                <img src= { YellowHalfStar }  alt="Half Star" />
-                            </div>
-                            <div className="star">
-                            </div>
-                        </div>
-                    </div>
-                    <div className="feedback">
-                        <h3>Lorem</h3>
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore facere culpa incidunt cum necessitatibus, minima minus voluptate quis. Eveniet consequatur ea ex illum est beatae distinctio iure excepturi tenetur soluta.</p>
-                        <div className="star-rating">
-                            <div className="star">
-                                <img src= { YellowStar }  alt="Star" />
-                            </div>
-                            <div className="star">
-                                <img src= { YellowStar }  alt="Star" />
-                            </div>
-                            <div className="star">
-                                <img src= { YellowStar }  alt="Star" />
-                            </div>
-                            <div className="star">
-                                <img src= { YellowStar }  alt="Star" />
-                            </div>
-                            <div className="star">
-                                <img src= { YellowStar }  alt="Half Star" />
+                    <Fade bottom cascade>
+                        <div className="feedback">
+                            <h3>Lorem</h3>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore facere culpa incidunt cum necessitatibus, minima minus voluptate quis. Eveniet consequatur ea ex illum est beatae distinctio iure excepturi tenetur soluta.</p>
+                            <div className="star-rating">
+                                <div className="star">
+                                    <img src= { YellowStar }  alt="Star" />
+                                </div>
+                                <div className="star">
+                                    <img src= { YellowStar }  alt="Star" />
+                                </div>
+                                <div className="star">
+                                    <img src= { YellowStar }  alt="Star" />
+                                </div>
+                                <div className="star">
+                                    <img src= { YellowHalfStar }  alt="Half Star" />
+                                </div>
+                                <div className="star">
+                                </div>
                             </div>
                         </div>
-                    </div>
-                    <div className="feedback">
-                        <h3>Lorem</h3>
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore facere culpa incidunt cum necessitatibus, minima minus voluptate quis. Eveniet consequatur ea ex illum est beatae distinctio iure excepturi tenetur soluta.</p>
-                        <div className="star-rating">
-                            <div className="star">
-                                <img src= { YellowStar }  alt="Star" />
+                    </Fade>
+                    <Fade bottom cascade>
+                        <div className="feedback">
+                            <h3>Lorem</h3>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore facere culpa incidunt cum necessitatibus, minima minus voluptate quis. Eveniet consequatur ea ex illum est beatae distinctio iure excepturi tenetur soluta.</p>
+                            <div className="star-rating">
+                                <div className="star">
+                                    <img src= { YellowStar }  alt="Star" />
+                                </div>
+                                <div className="star">
+                                    <img src= { YellowStar }  alt="Star" />
+                                </div>
+                                <div className="star">
+                                    <img src= { YellowStar }  alt="Star" />
+                                </div>
+                                <div className="star">
+                                    <img src= { YellowStar }  alt="Star" />
+                                </div>
+                                <div className="star">
+                                    <img src= { YellowStar }  alt="Half Star" />
+                                </div>
                             </div>
-                            <div className="star">
-                                <img src= { YellowStar }  alt="Star" />
-                            </div>
-                            <div className="star">
-                                <img src= { YellowStar }  alt="Star" />
-                            </div>
-                            <div className="star">
+                        </div>
+                    </Fade>
+                    <Fade bottom cascade>
+                        <div className="feedback">
+                            <h3>Lorem</h3>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore facere culpa incidunt cum necessitatibus, minima minus voluptate quis. Eveniet consequatur ea ex illum est beatae distinctio iure excepturi tenetur soluta.</p>
+                            <div className="star-rating">
+                                <div className="star">
+                                    <img src= { YellowStar }  alt="Star" />
+                                </div>
+                                <div className="star">
+                                    <img src= { YellowStar }  alt="Star" />
+                                </div>
+                                <div className="star">
+                                    <img src= { YellowStar }  alt="Star" />
+                                </div>
+                                <div className="star">
 
-                            </div>
-                            <div className="star">
+                                </div>
+                                <div className="star">
 
-                            </div>
-                        </div> 
-                    </div>
+                                </div>
+                            </div> 
+                        </div>
+                    </Fade>
                 </div>
-            </div>
+                </Fade>
 
+        </div>
+    </Fade>
 
 </div>
 }
