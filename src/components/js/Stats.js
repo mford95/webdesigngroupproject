aa90e54bbba2fd1eeb428c354c46c3ebc8d39e85
Rupert <rupert4t@gmail.com>
2020-06-25 15:57:07 +1200

    Link between pages added, content alignment fixed

--- src/components/js/Stats.js
+++ src/components/js/Stats.js
@@ -4,25 +4,23 @@ import "../css/Stats.css";
 
 export default function Stats() {
     return (
-        <div className="stats-section">
-            <div className="stats-text">
-                <div className="stats-individual">
-                    <h2>127</h2>
-                    <p>Student amount</p>
-                </div>
+        <div className="landing-stats">
 
-                <div className="stats-individual">
-                    <h2>A-</h2>
-                    <p>Average grade</p>
-                </div>
-
-                <div className="stats-individual">
-                    <h2>3yrs</h2>
-                    <p>Course length</p>
-                </div>
-
+            <div className="stats-indiv">
+                <h1>127</h1>
+                <h3>Student amount</h3>
             </div>
-            
+
+            <div className="stats-indiv">
+                <h1>A-</h1>
+                <h3>Average grade</h3>
+            </div>
+
+            <div className="stats-indiv">
+                <h1>3yrs</h1>
+                <h3>Course length</h3>
+            </div>
+
         </div>
     )
 }
